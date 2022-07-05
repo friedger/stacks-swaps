eb290270d7ba13aa7ea0f2f9c73b7b315e8c7473
aulneau <aulneau@users.noreply.github.com>
2022-07-05 14:30:26 -0500

    fix: auto import

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(() => ({
     loader: 'jsx',
     include: /src\/.*\.jsx?$/,
     exclude: [],
+    jsxInject: `import React from 'react'`,
   },
   build: {
     target: 'esnext',

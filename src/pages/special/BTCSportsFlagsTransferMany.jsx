2f97b8c91d497e23b555d51fa9a4f1bf7a4ac8a1
friedger <mail@friedger.de>
2023-06-22 15:45:29 +0200

    add description

--- src/pages/special/BTCSportsFlagsTransferMany.jsx
+++ src/pages/special/BTCSportsFlagsTransferMany.jsx
@@ -1,14 +1,14 @@
-import { useAccount, useOpenContractCall } from '@micro-stacks/react';
-import { Box, Button, Flex, Stack, Text } from '@stacks/ui';
+import { useAccount, useAuth, useOpenContractCall } from '@micro-stacks/react';
+import { Box, Button, Text } from '@stacks/ui';
 import { listCV, standardPrincipalCV, uintCV } from 'micro-stacks/clarity';
 import {
   NonFungibleConditionCode,
-  createAssetInfo,
   makeStandardNonFungiblePostCondition,
 } from 'micro-stacks/transactions';
 import { useEffect, useState } from 'react';
 import { nonFungibleTokensApi } from '../../lib/constants';
 import { points } from './BTCSportsFlags';
+import GetStartedButton from '../../components/GetStartedButton';
 
 function Flag({ flag, onSelect, selectedIds }) {
   const id = flag.value.repr.substr(1);
@@ -34,6 +34,7 @@ export default function BTCSportFlagsTransferMany() {
   const [total, setTotal] = useState();
   const [shown, setShown] = useState();
 
+  const { openAuthRequest } = useAuth();
   const { stxAddress } = useAccount();
   const onSelect = id => {
     if (ids.includes(id)) {
@@ -74,63 +75,74 @@ export default function BTCSportFlagsTransferMany() {
     <main className="container">
       <div>
         <h1>BTC Sports Flags/Coins Transfer Many</h1>
-        {flags ? (
+        Hello, use this transfer-many option to transfer flags in bulk to BTC Sports burn wallet
+        (btcsburn.btc) to exchange for promotions. Please go to{' '}
+        <a href="https://discord.gg/btcsports">https://discord.gg/btcsports</a> in order to find out
+        active promotions. Thanks!
+        <br />
+        {stxAddress ? (
           <>
-            Showing {shown} / {total}
+            {flags ? (
+              <>
+                Showing {shown} / {total}
+                <br />
+                <Box justifyContent="center" p="tight" columnGap="loose">
+                  {flags.map((flag, i) => {
+                    return <Flag key={i} flag={flag} onSelect={onSelect} selectedIds={ids} />;
+                  })}
+                </Box>
+              </>
+            ) : (
+              <>Loading your NFTs</>
+            )}
+            <br />
+            Selected {ids.length}
             <br />
             <Box justifyContent="center" p="tight" columnGap="loose">
-              {flags.map((flag, i) => {
-                return <Flag key={i} flag={flag} onSelect={onSelect} selectedIds={ids} />;
+              {ids.map((id, i) => {
+                return (
+                  <Text key={i} m="5px" background="yellow">
+                    {id}
+                  </Text>
+                );
               })}
             </Box>
+            <Button mode="tertiary" onClick={() => setIds([])}>
+              Clear Selection
+            </Button>
+            <br />
+            <br />
+            <Button
+              className="btn btn-outline-primary"
+              disabled={!stxAddress}
+              onClick={async () => {
+                await openContractCall({
+                  contractAddress: 'SPN4Y5QPGQA8882ZXW90ADC2DHYXMSTN8VAR8C3X',
+                  contractName: 'btc-sports-flags-many',
+                  functionName: 'transfer-many',
+                  functionArgs: [
+                    listCV(ids.map(id => uintCV(id))),
+                    standardPrincipalCV('SP15P146EH0NJB7KNGMWX1NB5EE8750ZM2ZB2BK5Z'),
+                  ],
+                  postConditions: ids.map(id =>
+                    makeStandardNonFungiblePostCondition(
+                      stxAddress,
+                      NonFungibleConditionCode.DoesNotOwn,
+                      'SP2BE8TZATXEVPGZ8HAFZYE5GKZ02X0YDKAN7ZTGW.btc-sports-flags-nft::btc-sports-flags',
+                      uintCV(id)
+                    )
+                  ),
+                });
+              }}
+            >
+              Transfer Selected Items
+            </Button>
+            <br />
+            {JSON.stringify(status)}
           </>
         ) : (
-          <>Loading your NFTs</>
+          <GetStartedButton openAuthRequest={openAuthRequest} />
         )}
-        <br />
-        Selected {ids.length}
-        <br />
-        <Box justifyContent="center" p="tight" columnGap="loose">
-          {ids.map((id, i) => {
-            return (
-              <Text key={i} m="5px" background="yellow">
-                {id}
-              </Text>
-            );
-          })}
-        </Box>
-        <Button mode="tertiary" onClick={() => setIds([])}>
-          Clear Selection
-        </Button>
-        <br />
-        <br />
-        <Button
-          className="btn btn-outline-primary"
-          disabled={!stxAddress}
-          onClick={async () => {
-            await openContractCall({
-              contractAddress: 'SPN4Y5QPGQA8882ZXW90ADC2DHYXMSTN8VAR8C3X',
-              contractName: 'btc-sports-flags-many',
-              functionName: 'transfer-many',
-              functionArgs: [
-                listCV(ids.map(id => uintCV(id))),
-                standardPrincipalCV('SP15P146EH0NJB7KNGMWX1NB5EE8750ZM2ZB2BK5Z'),
-              ],
-              postConditions: ids.map(id =>
-                makeStandardNonFungiblePostCondition(
-                  stxAddress,
-                  NonFungibleConditionCode.DoesNotOwn,
-                  'SP2BE8TZATXEVPGZ8HAFZYE5GKZ02X0YDKAN7ZTGW.btc-sports-flags-nft::btc-sports-flags',
-                  uintCV(id)
-                )
-              ),
-            });
-          }}
-        >
-          Transfer Selected Items
-        </Button>
-        <br />
-        {JSON.stringify(status)}
       </div>
     </main>
   );
